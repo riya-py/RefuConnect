a9da2860b5beb580ce03a58909a3d3eb9afef152
riya-py <riya.rk006@email.com>
2025-04-24 19:01:19 +0530

    added i18n

---
+++ src/i18n/i18n.js
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-language-detector';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: process.env.NODE_ENV === 'development',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        common: require('../../public/locales/en/common.json')
+      },
+      ar: {
+        common: require('../../public/locales/ar/common.json')
+      }
+    },
+    defaultNS: 'common'
+  });
+
+export default i18n;
